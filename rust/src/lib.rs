662d605836236105eb857a8b9391ba39e29e5785
Kyle Karpack <kylekarpack@gmail.com>
2023-05-21 21:48:38 -0700

    Rust improvements

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,8 +3,14 @@ use worker::*;
 
 #[event(fetch)]
 async fn main(req: Request, env: Env, ctx: Context) -> Result<Response> {
-    let url = "https://www.goodreads.com/review/list/63515611-kyle?order=d&amp;page=1&amp;shelf=read&amp;sort=date_read";
-    let response = reqwest::get(url)
+    let url = "https://www.goodreads.com/review/list/63515611-kyle?order=d&amp;page=1&amp;shelf=read&amp;sort=date_read&page=1";
+
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(url)
+        .header(reqwest::header::ACCEPT, "application/javascript")
+        .send()
         .await
         .expect("Could not fetch data")
         .text()
