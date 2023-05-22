5ed5125c61b5492315e681b9680e62e6bf788e47
Kyle Karpack <kylekarpack@gmail.com>
2023-05-21 21:15:20 -0700

    Working rust fetch

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,5 +2,8 @@ use worker::*;
 
 #[event(fetch)]
 async fn main(req: Request, env: Env, ctx: Context) -> Result<Response> {
-    Response::ok("Hello, World!")
+    let url = "https://www.goodreads.com/review/list/63515611-kyle?order=d&amp;page=1&amp;shelf=read&amp;sort=date_read";
+    let response = reqwest::get(url).await.unwrap().text().await.unwrap();
+
+    return Response::ok(response);
 }
