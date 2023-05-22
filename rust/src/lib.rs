f662461ccb5ff42c6bfc93811464a790c3f6d8e2
Kyle Karpack <kylekarpack@gmail.com>
2023-05-21 21:34:56 -0700

    Rust updates

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,9 +1,17 @@
+use scraper::*;
 use worker::*;
 
 #[event(fetch)]
 async fn main(req: Request, env: Env, ctx: Context) -> Result<Response> {
     let url = "https://www.goodreads.com/review/list/63515611-kyle?order=d&amp;page=1&amp;shelf=read&amp;sort=date_read";
-    let response = reqwest::get(url).await.unwrap().text().await.unwrap();
+    let response = reqwest::get(url)
+        .await
+        .expect("Could not fetch data")
+        .text()
+        .await
+        .expect("Could not get text from response");
 
-    return Response::ok(response);
+    let document = Html::parse_document(&response);
+
+    return Response::ok(document.html());
 }
